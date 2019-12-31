b64c7b4dd91281bc3990f7acef92ba043cc7bf02
thdtjsdn <thdtjsdn@gmail.com>
2019-12-31 21:17:21 +0900

    N

---
+++ WebPage/root/ui_template/Spinner/spinkit/6-chasing-dots.js
@@ -0,0 +1,180 @@
+//----------------------------------------------------------------------------------------------------;
+var fileNm = "ui/Spinner/spinkit/6-chasing-dots.js";
+if( console ) console.log( "[ S ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
+
+/**
+ * @class
+ * @param {String} url js file url
+ * @param {HTMLDIVElement} $el_div 이 객체가 조작할 HTMLElement
+ */
+(function( url, $el_div ){
+
+	//Extends;
+	var _this = window.b2linkUIClass.container.FullSizeLayout( url, $el_div );
+		_this.deactive_StopEvent();
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//	STATIC VARIABLES - 알파벳 순 정렬;
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//--------------------------------------------------public;
+
+	//--------------------------------------------------protected;
+
+	//--------------------------------------------------private;
+
+	//--------------------------------------------------;
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//	MEMBER VARIABLES - 알파벳 순 정렬;
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//--------------------------------------------------public;
+
+	//--------------------------------------------------protected;
+
+	//--------------------------------------------------private;
+
+	/**
+	 * targetElement의 ChildNodes중 입력한 className과 같은 자식 객체를 검색하여 0번째 객체를 반환한다.
+	 * @function
+	 * @param {HTMLElement} t HTMLElement
+	 * @param {String} className
+	 */
+	var $f0 = window.b2link.element.getElementByClassName;
+
+	//--------------------------------------------------;
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//	EVENT - addEvent***, removeEvent*** 셋트 배치 후 알파벳 순 정렬;
+	//	- Callback를 정의하는 곳이 아님.
+	//	- dispatchEvent로 인하여 EventType에 의거해서 구동될 EventListenr만 정의 하는 구역임
+	//	- Common Interface
+	//	_addEvent : 이 객체에서 addEventListener를 정의 한다.
+	//	_removeEvent : 이 객체에서 removeEventListener를 정의 한다.
+
+	//----------------------------------------------------------------------------------------------------;
+
+	/**
+	 * 이 Controller의 HTMLElement에 사용할 EventListener를 추가한다.
+	 * 무조건 등록할 예정인 이벤트를 한번 제거 후 등록한다.
+	 * @function
+	 */
+	var _addEvent = function()
+	{
+		_removeEvent();
+		window.addEventListener( "resize", _evt_resize$parentElement, false, 0, true );
+	};
+
+	/**
+	 * 이 Controller의 HTMLElement에 추가한 EventListener를 제거한다.
+	 * @function
+	 */
+	var _removeEvent = function()
+	{
+		window.removeEventListener( "resize", _evt_resize$parentElement, false );
+	};
+
+	/**
+	 * @function
+	 * @param {ResizeEvent} e
+	 */
+	var _evt_resize$parentElement = function( e )
+	{
+		window.TtwLog.timeStamp( "-- [ S ] - _evt_resize$parentElement():void----------" );
+
+		_setPosition();
+
+		window.TtwLog.timeStamp( "-- [ E ] - _evt_resize$parentElement():void----------" );
+	};
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//	FUNCTION - 알파벳 순 정렬;
+	//	- Common Interface
+	//	_dispose : 이 객체의 removeEvent를 실행하고 이 객체가 보유한 dispose가 있는 인스턴스들의 dispose를 호출해준다.
+
+	//----------------------------------------------------------------------------------------------------;
+
+	/**
+	 * 이 Controller(_this)와 TargetElement(_this.__el)를 GC가 정상적으로 메모리에서 완전히 제거될수 있는 상태를 만든다.
+	 * Reference Count를 0으로 만드는 행위를 한다.
+	 * 각종 인스턴트 = null처리
+	 * 모든 EventListener 제거
+	 * 특정 자료구조에서 참조값 제거
+	 * @override
+	 * @function
+	 */
+	var _dispose = function()
+	{
+		_removeEvent();
+
+		_dispose.super();
+	};
+	_dispose.super = _this.dispose;
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//	GETTER / SETTER - GET METHOD, SET METHOD, GET/SET set METHOD, GETTER Property, SETTER Property, GETTER/SETTER Property, 구간 안에서 알파벳 정렬;
+	//	- Common Interface
+	//	_setData : 이 객체가 정상적으로 작동되기 위한 값들을 주입 받는 다.
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//--------------------------------------------------GET;
+
+	//--------------------------------------------------SET;
+
+	//----------------------------------------------------------------------------------------------------;
+
+	/**
+	 * @function
+	 */
+	var _setPosition = function()
+	{
+		window.TtwLog.timeStamp( "-- [ S ] - _setPosition():void----------" );
+
+		window.TtwLog.timeStamp( "-- [ E ] - _setPosition():void----------" );
+	};
+
+	/**
+	 * @function
+	 */
+	var _setData = function( result )
+	{
+		window.TtwLog.timeStamp( "-- [ S ] - _setData():void----------" );
+
+		window.TtwLog.timeStamp( "-- [ E ] - _setData():void----------" );
+	};
+
+	//--------------------------------------------------GET / SET;
+
+	//--------------------------------------------------GETTER;
+
+	//--------------------------------------------------SETTER;
+
+	//--------------------------------------------------GETTER / SETTER;
+
+	//--------------------------------------------------;
+
+	//----------------------------------------------------------------------------------------------------;
+
+	//----------;
+	_addEvent();
+	//----------;
+
+	//--------------------------------------------------this - FUNCTION, GET METHOD, SET METHOD, GET/SET METHOG, GETTER, SETTER, GETTER / SETTER 순서 - 타입별 내부는 알파벳 순서 _는 알파벳보다 빠르다.;
+	var _ = _this;
+
+		_.__el = $el_div;
+
+		_.dispose = _dispose;
+	//--------------------------------------------------this;
+	return _;
+});
