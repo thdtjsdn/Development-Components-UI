73743fd1e7f3adfd16a1b3b6aeb4c5b28590da2f
thdtjsdn <thdtjsdn@gmail.com>
2020-01-02 03:44:06 +0900

    Update HTTPServer_WebPage.bat

--- HTTPServer_WebPage/HTTPServer_WebPage.bat
+++ HTTPServer_WebPage/HTTPServer_WebPage.bat
@@ -1,1 +1,1 @@
-..\..\Development-Binary-Windows\Binary\NodeJS\exes100.exe --expose-gc "HTTPServer_WebPage.js"
+..\..\TtwPlatform-Binary--Win\Binary\NodeJS\exes100.exe --expose-gc "HTTPServer_WebPage.js"
