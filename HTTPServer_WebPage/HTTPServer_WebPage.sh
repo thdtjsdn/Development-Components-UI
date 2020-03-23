ccf83ec72b955c3ef59a96e45c90333517fa975f
thdtjsdn <thdtjsdn@gmail.com>
2020-03-23 22:03:31 +0900

    Update HTTPServer_WebPage.sh

--- HTTPServer_WebPage/HTTPServer_WebPage.sh
+++ HTTPServer_WebPage/HTTPServer_WebPage.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 kill -9 `lsof -i TCP:49323 | awk '/LISTEN/ {print $2}'`
 
 #..\..\TtwPlatform-Binary--Linux-Ubuntu/Binary/NodeJS/exes100 --expose-gc ./HTTPServer_WebPage.js 49323
-exes100 HTTPServer_WebPage.js 49323
+exes100 /data/data/com.termux/files/home/Application/Development-Components-UI/HTTPServer_WebPage/HTTPServer_WebPage.js 49323
